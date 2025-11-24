3737e5f67e1b1090c8616e47105443ba30069444
Vega <alexvega20137@gmail.com>
2025-11-24 13:56:19 -0500

    Added the regular
    Created more regular counting

--- GitKrakenTwo.java
+++ GitKrakenTwo.java
@@ -5,7 +5,7 @@ public class GitKrakenTwo {
 		ExecutorService exe = Executors.newFixedThreadPool(1000);
 		Callable<Integer> callable = () -> {
 			int total = 0;
-			for(int i=0; i<1000000; i++)
+			for(int i=0; i<1000000000; i++)
 				total++;
 			return total;
 		};
@@ -15,6 +15,14 @@ public class GitKrakenTwo {
 		System.out.println("Result from Future: "+result);
 		exe.shutdown();
 		long duration = System.nanoTime()-startTime;
+		System.out.println("Duration: "+duration/1000000000.0+" seconds");
+
+		startTime = System.nanoTime();
+		int newTotal = 0;
+		for(int i=0; i<1000000000; i++)
+			newTotal++;
+
+		duration = System.nanoTime()-startTime;
 		System.out.println("Duration: "+duration/1000000000.0+" seconds");
 		System.out.println("Completed");
 	}
