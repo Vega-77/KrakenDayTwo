0d938437407ecf56d28404f2e31a3204fbfbcd7d
Vega <alexvega20137@gmail.com>
2025-11-24 13:49:34 -0500

    Actually wrote code
    Did some cool stuff with Executions

--- GitKrakenTwo.java
+++ GitKrakenTwo.java
@@ -1,6 +1,17 @@
 import java.util.concurrent.*;
 public class GitKrakenTwo {
-	public static void main(String[]args){
-		ExecutorService exce = Executors.newFixedThreadPool(1000);
+	public static void main(String[]args) throws Exception{
+		ExecutorService exe = Executors.newFixedThreadPool(1000);
+		Callable<Integer> callable = () -> {
+			int total = 0;
+			for(int i=0; i<1000000; i++)
+				total++;
+			return total;
+		};
+
+		Future<Integer> future = exe.submit(callable);
+		int result = future.get();
+		System.out.println("Result from Future: "+result);
+		exe.shutdown();
 	}
 }
