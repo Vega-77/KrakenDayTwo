eb90c8eb3a6b834138075891f10813b902092a0f
Vega <alexvega20137@gmail.com>
2025-11-24 13:35:59 -0500

    Finalized timer
    Finished the timer branch

--- GitKrakenTwo.java
+++ GitKrakenTwo.java
@@ -3,5 +3,6 @@ public class GitKrakenTwo {
 		long startTime = System.nanoTime();
 
 		long duration = System.nanoTime()-startTime;
+		System.out.println("Duration: "+duration/1000000000.0+" seconds");
 	}
 }
